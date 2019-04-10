465e4bf11277faad8c2cc02c909e16eda47a2d51
suzumi <ganimata39@gmail.com>
2019-04-10 19:00:20 +0900

    Fix bot config

--- conf/dev.js
+++ conf/dev.js
@@ -2,8 +2,8 @@ module.exports.dev = {
     slack: {
         token: process.env.SLACK_TOKEN,
         bot: {
-            icon_url: 'https://octodex.github.com/images/original.png',
-            username: 'スケジューリングするタコ猫'
+            icon_url: 'https://octodex.github.com/images/daftpunktocat-thomas.gif',
+            username: 'devねこ'
         },
         postChannel: 'test-private-channel'
     },
--- conf/prod.js
+++ conf/prod.js
@@ -2,8 +2,8 @@ module.exports.prod = {
     slack: {
         token: process.env.SLACK_TOKEN,
         bot: {
-            icon_url: 'https://octodex.github.com/images/original.png',
-            username: 'スケジューリングするタコ猫'
+            icon_url: 'https://octodex.github.com/images/privateinvestocat.jpg',
+            username: '闇の裁定者'
         },
         postChannel: 'test-private-channel' // FIXME
     },
